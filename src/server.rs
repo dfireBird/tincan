f937d35522ecff66b1bd24415febb5917e3bf5fc
dfireBird <me@dfirebird.codes>
2021-01-30 10:57:45 +0530

    feat: implement file message handler

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,6 @@
-use std::io::Read;
+use dirs;
+use std::fs;
+use std::io::{Read, Write};
 use std::net::TcpStream;
 use std::str;
 use std::sync::mpsc::Sender;
@@ -20,7 +22,7 @@ fn recv_messages(peer: &mut TcpStream, tx: &Sender<(Message, Vec<u8>)>) {
 
         let message_type = str::from_utf8(&message_type).unwrap();
         match message_type {
-            "file" => unimplemented!(), // handle file
+            "file" => handle_file(peer, &message),
             "chat" => handle_chat(&message, tx),
             _ => continue,
         }
@@ -30,4 +32,25 @@ fn recv_messages(peer: &mut TcpStream, tx: &Sender<(Message, Vec<u8>)>) {
 fn handle_chat(message: &Vec<u8>, tx: &Sender<(Message, Vec<u8>)>) {
     // Send the chat message to UI thread
     tx.send((Message::Chat, message.clone())).unwrap();
+}
+
+fn handle_file(peer: &mut TcpStream, file_data: &Vec<u8>) {
+    // Split at 96th byte to get the file name
+    let (file_name, file_data) = file_data.split_at(96);
+
+    // If file name if utf8 then write the file or send the error to the other peer
+    if let Ok(file_name) = str::from_utf8(file_name) {
+        let mut file_path = dirs::download_dir().unwrap();
+        file_path.push(file_name);
+        fs::write(&file_path.as_path(), file_data).unwrap();
+    } else {
+        let error = "File name is not UTF-8 compatabile";
+        let error_msg = [
+            "Error".as_bytes(),
+            &error.as_bytes().len().to_be_bytes(),
+            error.as_bytes(),
+        ]
+        .concat();
+        peer.write(&error_msg).unwrap();
+    }
 }
