cf4fd6cb285afb3e02ee28c91318b7e1dd0cf39e
dfireBird <me@dfirebird.codes>
2021-02-04 12:02:43 +0530

    refactor: move drawing on terminal into a function

--- src/ui.rs
+++ src/ui.rs
@@ -39,53 +39,7 @@ pub fn start_ui() -> Result<(), Box<dyn Error>> {
 
     // UI loop
     loop {
-        terminal.draw(|f| {
-            let chunks = Layout::default()
-                .direction(Direction::Vertical)
-                .margin(2)
-                .constraints(
-                    [
-                        Constraint::Length(1),
-                        Constraint::Min(1),
-                        Constraint::Length(3),
-                    ]
-                    .as_ref(),
-                )
-                .split(f.size());
-
-            // Help Message
-            let (msg, style) = (
-                vec![
-                    Span::raw("Press "),
-                    Span::styled("Ctrl-D", Style::default().add_modifier(Modifier::BOLD)),
-                    Span::raw(" to exit."),
-                ],
-                Style::default().add_modifier(Modifier::RAPID_BLINK),
-            );
-            let mut help_text = Text::from(Spans::from(msg));
-            help_text.patch_style(style);
-            f.render_widget(Paragraph::new(help_text), chunks[0]);
-
-            // Messages Block
-            let messages: Vec<ListItem> = state
-                .messages
-                .iter()
-                .enumerate()
-                .map(|(i, m)| {
-                    let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
-                    ListItem::new(content)
-                })
-                .collect();
-            let messages =
-                List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
-            f.render_widget(messages, chunks[1]);
-
-            // Input Block
-            let input = Paragraph::new(state.input.as_ref())
-                .style(Style::default().fg(Color::Yellow))
-                .block(Block::default().borders(Borders::ALL).title("Input"));
-            f.render_widget(input, chunks[2]);
-        })?;
+        draw_ui(&mut terminal, &state)?;
 
         // Handle Input
         if let Event::Input(input) = events.next()? {
@@ -125,5 +79,59 @@ fn terminal_deinitialization(
         DisableMouseCapture
     )?;
     terminal.show_cursor()?;
+    Ok(())
+}
+
+fn draw_ui(
+    terminal: &mut Terminal<CrosstermBackend<Stdout>>,
+    state: &State,
+) -> Result<(), Box<dyn Error>> {
+    terminal.draw(|f| {
+        let chunks = Layout::default()
+            .direction(Direction::Vertical)
+            .margin(2)
+            .constraints(
+                [
+                    Constraint::Length(1),
+                    Constraint::Min(1),
+                    Constraint::Length(3),
+                ]
+                .as_ref(),
+            )
+            .split(f.size());
+
+        // Help Message
+        let (msg, style) = (
+            vec![
+                Span::raw("Press "),
+                Span::styled("Ctrl-D", Style::default().add_modifier(Modifier::BOLD)),
+                Span::raw(" to exit."),
+            ],
+            Style::default().add_modifier(Modifier::RAPID_BLINK),
+        );
+        let mut help_text = Text::from(Spans::from(msg));
+        help_text.patch_style(style);
+        f.render_widget(Paragraph::new(help_text), chunks[0]);
+
+        // Messages Block
+        let messages: Vec<ListItem> = state
+            .messages
+            .iter()
+            .enumerate()
+            .map(|(i, m)| {
+                let content = vec![Spans::from(Span::raw(format!("{}: {}", i, m)))];
+                ListItem::new(content)
+            })
+            .collect();
+        let messages =
+            List::new(messages).block(Block::default().borders(Borders::ALL).title("Messages"));
+        f.render_widget(messages, chunks[1]);
+
+        // Input Block
+        let input = Paragraph::new(state.input.as_ref())
+            .style(Style::default().fg(Color::Yellow))
+            .block(Block::default().borders(Borders::ALL).title("Input"));
+        f.render_widget(input, chunks[2]);
+    })?;
     Ok(())
 }
