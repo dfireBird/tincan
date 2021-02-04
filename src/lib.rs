67159d49a2e7cb22f600facd4a8a6ce94b17be40
dfireBird <me@dfirebird.codes>
2021-02-04 10:47:51 +0530

    chore: spawn a thread to run messages

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,5 @@ mod events;
 pub mod server;
 mod state;
 pub mod ui;
+
+pub const DEFAULT_PORT: u16 = 42069;
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,28 @@
 use std::error::Error;
+use std::net::{Ipv4Addr, TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
 
+use tincan::server;
 use tincan::ui;
+use tincan::DEFAULT_PORT;
+
 fn main() -> Result<(), Box<dyn Error>> {
+    let listener = TcpListener::bind((Ipv4Addr::UNSPECIFIED, DEFAULT_PORT))?;
+    let (tx, rx) = mpsc::channel();
+    let server_handle = thread::spawn(move || {
+        let mut peer: TcpStream;
+        loop {
+            peer = match listener.accept() {
+                Ok((peer, _)) => peer,
+                Err(_) => continue,
+            };
+            match server::handshake(&mut peer, &tx) {
+                Some(_) => break,
+                None => continue,
+            }
+        }
+        server::recv_messages(&mut peer, &tx);
+    });
     ui::start_ui()
 }
